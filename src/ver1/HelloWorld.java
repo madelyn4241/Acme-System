6b034563cdda61fa257a9308db6ed07fb8deaac0
Maddi <maddi.a42436@gmail.com>
2024-06-27 15:14:00 -0400

    made hey now class

--- src/ver1/HelloWorld.java
+++ src/ver1/HelloWorld.java
@@ -4,7 +4,10 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+	}
+	public static void heyNow() {
+		String Msg = "Hey Now!";
+		System.out.println(Msg);
 	}
 
 }
